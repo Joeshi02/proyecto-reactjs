328950d355ab918cbc5beb49772742af3bd53fd6
joacoEscobar <joaco.escobar.h@gmail.com>
2023-08-19 16:28:11 -0300

    incorporamos icono de carrito

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg bg-light p-0">
             <div class="container-fluid color">
                 <a class="navbar-brand" href="#"><image><img src="../images_limit/agua_import.png" class = "logo img-fluid" alt="logo" /> </image></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,6 +22,9 @@ const NavBar = () => {
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">CONTACTO</a>
+                        </li>
+                        <li className="nav-item">
+                            <a class="nav-link pt-1" href="#"><img src="../images_limit/carrito-de-compras.png" alt="carrito"/></a>
                         </li>
                     </ul>
                 </div>
