51f28a8c6e6bc94cfe51746a7dde72cbddd2f2b6
joacoEscobar <joaco.escobar.h@gmail.com>
2023-09-25 19:04:34 -0300

    agregamos firebase1

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,31 +1,40 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import productsJSON from "../JSON/products.json"
+// import productsJSON from "../JSON/products.json"
 import ItemDetail from "../ItemDetail/ItemDetail";
+import {doc, getDoc, getFirestore} from "firebase/firestore"
 
-const detailAPI = (itemId) => {
-    return new Promise ((resolve, reject) => {
-        setTimeout(() =>{
-            if(itemId !== undefined){
-                const filtro = productsJSON.find(
-                    (item) => item.id == itemId
-                );
-                resolve(filtro)
-            }else{
-                resolve(productsJSON)
-            }
-        },100);
-    });
-};
+// const detailAPI = (itemId) => {
+//     return new Promise ((resolve, reject) => {
+//         setTimeout(() =>{
+//             if(itemId !== undefined){
+//                 const filtro = productsJSON.find(
+//                     (item) => item.id == itemId
+//                 );
+//                 resolve(filtro)
+//             }else{
+//                 resolve(productsJSON)
+//             }
+//         },100);
+//     });
+// };
 
 export default function ItemDetailContainer ({}) {
     const [products , setProducts] = useState ([]);
     const {itemId} = useParams ()
     
     useEffect (() => {
-        detailAPI (itemId)
-        .then((data) => setProducts (data));
-    },[itemId]);
+        const db = getFirestore()
+        const itemRef = doc(db, "products", itemId)
+        getDoc(itemRef).then((snapshot) => {
+            setProducts({id:snapshot.id, ...snapshot.data()})
+        })
+    },[itemId])
+
+    // useEffect (() => {
+    //     detailAPI (itemId)
+    //     .then((data) => setProducts (data));
+    // },[itemId]);
 
     return (
         <div className="d-flex justify-content-center">
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,32 +1,50 @@
 import { useEffect, useState } from "react";
-import productsJSON from "../JSON/products.json"
+// import productsJSON from "../JSON/products.json"
 import ItemList from "../ItemList/ItemList"
 import { useParams } from "react-router-dom";
+import {collection, getDocs, getFirestore, query, where} from "firebase/firestore"
 
-const mockAPI = (categoryName) => {
+// const mockAPI = (categoryName) => {
 
-    return new Promise ((resolve, reject) =>{
-        setTimeout(() =>{
-            if(categoryName !== undefined) {
-                const filtro = productsJSON.filter(
-                    (item) => item.category === categoryName
-                );
-            resolve(filtro)
-            }else{
-                resolve(productsJSON);
-            }
+//     return new Promise ((resolve, reject) =>{
+//         setTimeout(() =>{
+//             if(categoryName !== undefined) {
+//                 const filtro = productsJSON.filter(
+//                     (item) => item.category === categoryName
+//                 );
+//             resolve(filtro)
+//             }else{
+//                 resolve(productsJSON);
+//             }
             
-        },100);
-    });
-};
+//         },100);
+//     });
+// };
 
 export default function ItemListContainer  ({greeting}) {
     const [products, setProducts] = useState ([]);
     const {categoryName} = useParams ()
+    useEffect (() => {
+        const db = getFirestore()
 
-    useEffect(() => {
-        mockAPI(categoryName).then((data) => setProducts(data));
-    },[categoryName]);
+        const productsRef = categoryName 
+        ? query(collection(db, "products"), where("category","==", categoryName))
+        : collection (db, "products") 
+
+        getDocs(productsRef).then ((snapshot) => {
+                setProducts(
+                    snapshot.docs.map((doc) => {
+                        return {id: doc.id, ...doc.data()}
+                    })
+                )
+            
+        })
+    }, [categoryName])
+
+
+    // useEffect(() => {
+    //     mockAPI(categoryName).then((data) => setProducts(data));
+    // },[categoryName]);
     
     return(
         <div>
